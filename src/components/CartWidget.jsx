28c5cf5babc7040edef487561e06ffb9e38e7db9
MatiCantora <cantoramatias@gmail.com>
2022-02-10 19:32:03 -0300

    v1.7

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,11 +1,16 @@
 import React from "react"
 import "../App.css"
 
+function click(e) {
+	e.preventDefault()
+	console.log("Compraste")
+}
+
 const Cart = () => {
 	return (
-		<div>
+		<button className="cart" onClick={click}>
 			<i className="fa-solid fa-cart-shopping"></i>
-		</div>
+		</button>
 	)
 }
 
