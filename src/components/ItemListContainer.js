985ffc253c86a245879b2f16c98608b7ca2d8395
MatiCantora <cantoramatias@gmail.com>
2022-02-15 20:46:13 -0300

    v1.9

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,7 +1,18 @@
-import React from "react"
+import { ItemCount } from "./ItemCount"
 
 const ItemListContainer = (props) => {
-	return <p>Bienvenido al Carrito, tienes {props.items} productos</p>
+	const handleCount = (cant) => {
+		alert(`Se agregaron ${cant} items`)
+	}
+
+	return (
+		<>
+			<p>Bienvenido al Carrito, tienes {props.items} productos</p>
+			<div className="count">
+				<ItemCount initial={1} stock={10} onAdd={handleCount} />
+			</div>
+		</>
+	)
 }
 
 export default ItemListContainer
